f11bdcd8e389315eb85f61bfff437eaeae6781f2
Samuel Dindyal <sam.dindyal@icloud.com>
2016-03-07 13:36:49 -0500

    Implements hex colour functionality

--- assets/colourpreview.js
+++ assets/colourpreview.js
@@ -14,6 +14,7 @@ var currentColour;
 $(function() {
   currentColour = baseColours[parseInt(Math.random() * baseColours.length)];
   setColours();
+  displayColours();
 });
 
 function colour(r, g, b) {
@@ -22,23 +23,35 @@ function colour(r, g, b) {
     this.red          = r;
     this.green        = g;
     this.blue         = b;
-    this.hex          = "" + r.toString(16) + g.toString(16) + b.toString(16);
+    var rString = ((r<16) ? '0' : '') + this.red.toString(16);
+    var gString = ((g<16) ? '0' : '') + this.green.toString(16);
+    var bString = ((b<16) ? '0' : '') + this.blue.toString(16);
+    this.hex          = rString + gString + bString;
     this.accentValue  = parseInt(Math.abs(255 - (((r + g + b)/3) + Math.max(r, Math.max(g, b))/2)));
+    this.rgbString    = 'rgb(' + this.red + ',' + this.green + ',' + this.blue + ')';
   }
 }
 
 function hexColour(hex) {
   var matches = [];
   if (hex.length == 6)
-    matches = hex.match(/.{1,2}/g);
+    matches = hex.match(/[a-fA-F0-9]{1,2}/g);
   else if (hex.length == 3)
-    matches = hex.match(/.{1}/g);
+    matches = hex.match(/[a-fA-F0-9]{1}/g);
+  else return;
   this.red = parseInt(matches[0], 16);
   this.green = parseInt(matches[1], 16);
   this.blue = parseInt(matches[2], 16);
+  this.hex = hex;
+  this.rgbString    = 'rgb(' + this.red + ',' + this.green + ',' + this.blue + ')';
   if (this.red <= 255 && this.green <= 255 && this.blue <= 255)
-    this.accentValue  = 255 - (((this.red + this.green + this.blue)/3)
-      + Math.max(this.red, Math.max(this.green, this.blue))/2);
+    this.accentValue = parseInt(Math.abs(255 - (((this.red + this.green + this.blue)/3)
+      + Math.max(this.red, Math.max(this.green, this.blue))/2)));
+}
+
+function displayColours() {
+  $('input#rgb').attr('value', currentColour.rgbString);
+  $('input#hex').attr('value', '#' + currentColour.hex);
 }
 
 function setColours() {
@@ -56,5 +69,25 @@ function setColours() {
 
   $('.text').css({"color": accentColourString});
   $('input.text').css({"border-color": accentColourString});
-
 }
+
+$('input#hex').on('keyup', function() {
+  if (this.value === '')
+    this.value = '#'
+  else if ((this.value.length == 4 || this.value.length == 7) && this.value.substring(1).match(/([A-Fa-f0-9])/)) {
+    currentColour = new hexColour(this.value.substring(1));
+    console.log(currentColour.hex);
+    setColours();
+    displayColours();
+  }
+});
+
+$('input#hex').on('focus', function() {
+    if (this.value === "")
+      this.value = '#';
+});
+
+$('input#hex').on('blur', function() {
+  this.value = '#' + currentColour.hex
+  displayColours();
+});
